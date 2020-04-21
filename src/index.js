63201a46228d22609171f191e9410d43a0de74a4
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-04-21 10:54:18 +0200

    Add slider section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-import imgFunction from './script.js'
+//import script from './script.js'
 import './styles/style.scss'
 
-//imgFunction()
+document.addEventListener('DOMContentLoaded', function() {
+  // slider
+  const gap = 30;
+
+  const carousel = document.querySelector('.slider__body'),
+    content = document.querySelector('.slider__content'),
+    next = document.querySelector('.slider__button--next'),
+    prev = document.querySelector('.slider__button--prev');
+
+  next.addEventListener('click', e => {
+    carousel.scrollBy(width + gap, 0);
+    if (carousel.scrollWidth !== 0) {
+      prev.style.display = 'block';
+    }
+    if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+      next.style.display = 'none';
+    }
+  });
+  prev.addEventListener('click', e => {
+    carousel.scrollBy(-(width + gap), 0);
+    if (carousel.scrollLeft - width - gap <= 0) {
+      prev.style.display = 'none';
+    }
+    if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+      next.style.display = 'block';
+    }
+  });
+
+  let width = carousel.offsetWidth;
+  window.addEventListener('resize', e => (width = carousel.offsetWidth));
+});
--- src/script.js
+++ src/script.js
@@ -3,13 +3,5 @@ import logo_hero from './assets/logo-hero.png'
 import bag from './assets/bag.png'
 import user from './assets/user.png'
 import menu from './assets/hamburger.png'
-/* import daftcodeImgUrl from './assets/daftcode.png'
-
-export default function() {
-  const section = document.createElement('section')
-  const img = document.createElement('img')
-  img.src = daftcodeImgUrl
-  section.appendChild(img)
-  document.body.appendChild(section)
-}
- */
+import arrow_right from './assets/next.png'
+import arrow_left from './assets/previous.png'
