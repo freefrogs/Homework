566f4168bb2cb64cd855b4e6c8c5c662f4e4ae80
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-04-27 16:08:07 +0200

    Add all products button handling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 import slider from './slider'
 import scrollTop from './scrollTop'
+import allProducts from './allProducts'
 
 const startFunction = () => {
   slider();
   scrollTop();
+  allProducts();
 };
 
 export default startFunction;
